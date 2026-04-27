cd675cce8b71ecde77285de1893c25dfde9c1291
bigechapo <lileroc123@gmail.com>
2026-04-27 09:27:17 -0500

    changes to all pages

--- obj/Debug/net8.0/SuazoPlumbing.AssemblyInfo.cs
+++ obj/Debug/net8.0/SuazoPlumbing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuazoPlumbing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e509438a8095bcf567e0e2f5df4b2b899361d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c137b38f7d97af46974b48500756324288a048")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuazoPlumbing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuazoPlumbing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
