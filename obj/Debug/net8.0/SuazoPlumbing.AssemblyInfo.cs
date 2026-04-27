47e509438a8095bcf567e0e2f5df4b2b899361d5
Eric Rafael Chavez <lileroc123@gmail.com>
2026-04-26 19:17:42 -0500

    added borders to all logos

--- obj/Debug/net8.0/SuazoPlumbing.AssemblyInfo.cs
+++ obj/Debug/net8.0/SuazoPlumbing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuazoPlumbing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a6f2af85a7302795255e30a28ca0bf63ec8dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5824ae4de87eed86f97599c9b75682d6867392")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuazoPlumbing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuazoPlumbing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
